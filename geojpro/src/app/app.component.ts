2e3343c245b284f8ac2c7076317fcd97c30e966c
Andrewmarac <marasigan.joshua.andrew@iisgalvanimi.edu.it>
2020-04-08 15:43:55 +0000

    creazione marker.model.ts

--- geojpro/src/app/app.component.ts
+++ geojpro/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import{Marker} from './models/marker.model';
+import {GEOJSON, GeoFeatureCollection } from './models/geojson.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'geojpro';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
+  title = 'ang-maps';
+  // google maps zoom level
+  zoom: number = 8;
+  geoJsonObject : GeoFeatureCollection; //Oggetto che conterrà il vettore di GeoJson
+  fillColor: string = "#FF0000";  //Colore delle zone catastali
+  markers: Marker[];  //Vettore con tutti i marker
+
+  lng: number = 9.02657833507687;
+  lat: number = 45.512051524024308;
+
+  constructor()
+  {
+    //Questi dati dovremmo scaricarli dal server, per ora li abbiamo copiati nel file gojson.model.ts
+    this.geoJsonObject = GEOJSON;
+    console.log(this.geoJsonObject); //stampo l'oggetto geoJsonObject sulla console
+    //Provo a visualizzare le coordinate della prima features
+    //console.log(?????);
+  }
+
 }
